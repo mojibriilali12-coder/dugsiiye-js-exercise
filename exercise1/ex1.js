5b088885a7271210772cc4d369ed1db8d1d3729b
isoorapp <isoorapp@gmail.com>
2026-01-02 19:15:54 -0600

    EXERCISE1 JS

---
+++ exercise1/ex1.js
@@ -0,0 +1,37 @@
+
+// ADDITIOM
+
+let x = 7;
+let y = 3;
+
+console.log(7+3);
+
+
+// SUBSTRACTION
+
+console.log(7-3);
+
+// MULTIPLICATION
+
+console.log(7*3);
+
+
+// divide
+
+console.log(7/3);
+
+// MODULU
+
+console.log(7%3);
+
+// INCREMENT AND DECREMENT
+
+// pre
+
+console.log(++x)
+
+//post
+
+console.log(x++)
+
+console.log(x);
