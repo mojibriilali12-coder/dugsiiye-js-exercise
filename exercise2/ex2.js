87b5c3b7579dfb22baf1cda5cfd4af80e93aa073
isoorapp <isoorapp@gmail.com>
2026-01-03 19:52:04 -0600

    added exercise 2

---
+++ exercise2/ex2.js
@@ -0,0 +1,41 @@
+
+// COMPARISON OPERATORS
+
+let a = 10;
+let b = "10";
+
+// EQUAL TO
+console.log("EQUAL TO")
+console.log(a == b);
+
+// NOT EQAUL TO
+console.log("NOT EQUAL TO")
+
+console.log(a !== b);
+
+// STRICTLY EQUAL TO
+console.log("STRICTLY EQUAL")
+console.log(a === b);
+
+// STRICTLY NOT EQUAL TO
+console.log("STRICT NOT EQAUL TO")
+console.log(a !== b);
+
+
+// GREATER THAN, LESS THAN, OR EQAUL
+
+// greater than
+console.log("GREATER THAN")
+console.log(a > b);
+
+// less than
+console.log("LESS THAN")
+console.log(a < b);
+
+// greater than or equal
+console.log("GREATER THAN OR EQUAL TO");
+console.log(a >= b);
+
+// less than or equal to
+console.log("LESS THAN OR EQUAL TO")
+console.log(a <= b);
