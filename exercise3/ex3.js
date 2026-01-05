272ffeb6202a340b81adc8f7eabac62d0a83eb16
isoorapp <isoorapp@gmail.com>
2026-01-04 19:11:45 -0600

    added exercise 3

---
+++ exercise3/ex3.js
@@ -0,0 +1,22 @@
+// LOGICAL OPERATORS
+
+// and && operator
+let isSunny = true;
+let isWekend = false;
+
+console.log("AND && OPERATOR");
+console.log(isSunny && isWekend);
+
+// or || operator
+
+console.log("OR || OPERATOR");
+
+console.log(isSunny || isWekend);
+
+// invert
+
+console.log("INVERT !");
+
+console.log(!isSunny)
+
+console.log(!isWekend);
