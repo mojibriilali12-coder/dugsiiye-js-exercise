d56e158f6cd4de53b9281dfb0784c591aeb51519
isoorapp <isoorapp@gmail.com>
2026-01-20 18:27:16 -0600

    exercise 5

---
+++ exercise5/ex5.js
@@ -0,0 +1,24 @@
+console.log("BASIC FUNCTION")
+const add =  (price, quantity)=> {
+    return price * quantity;
+}
+
+let totalka= add(100,2);
+let tiroguud=add(40,6)
+console.log(tiroguud)
+console.log(totalka);
+console.log(totalka / 2)
+
+
+// function expression
+
+console.log("FUNCTION EXPRESSION")
+
+const kudar =  (price, quantity)=>{
+     return price * quantity;
+}
+
+let sum= kudar(100,2);
+let sum1= kudar(50,2);
+console.log(sum);
+console.log(sum1)
