20a50e4003b0da69b7007c6269e27c98de2a2e5f
isoorapp <isoorapp@gmail.com>
2026-01-24 18:53:09 -0600

    added exercise10

---
+++ exercise10/arrayobject.js
@@ -0,0 +1,22 @@
+
+let library = [
+    {
+       title:"Atomic habits",
+       Author:"Davies",
+       year:2020,
+
+    },
+
+    {
+        title:"What is a main",
+        author:"John",
+        year:2005,
+    }
+]
+
+console.log('displayed first book title')
+console.log(library[0].title)
+
+console.log('displayed second book Author');
+
+console.log(library[1].author)
