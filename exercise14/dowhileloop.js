770739b5bb3b0943bbe6c326cae0d445a9e684c3
isoorapp <isoorapp@gmail.com>
2026-01-28 18:14:39 -0600

    added exercise15

---
+++ exercise14/dowhileloop.js
@@ -0,0 +1,10 @@
+
+
+let password;
+
+do{
+    password=prompt("Please enter 12 numbers")
+    
+}while(password !== "123456789111");
+
+console.log("welcome")
---
+++ exercise15/forloopofin.js
@@ -0,0 +1,20 @@
+
+
+let people=[
+       { name:"geedi", age:30, city:"hargaysa" },
+       
+       { name:"geedi",age:30, city:"hargaysa"},
+       { name:"geedi",age:30, city:"hargaysa"}
+
+];
+
+for(let key in people){
+console.log("---")
+  console.log(people[key])
+}
+
+for(let pep of people){
+    console.log("---")
+    console.log(pep)
+}
+
