add587ae83cabf40b6d8e10b3e37b8e505f74078
isoorapp <isoorapp@gmail.com>
2026-01-29 18:29:40 -0600

    added exercise17

---
+++ exercise17/elseif.js
@@ -0,0 +1,17 @@
+
+
+let temperture= -1;
+
+if(temperture < 0){
+    console.log("very cold")
+
+}else if(temperture <= 15){
+   console.log("cold")
+
+}else if(temperture >= 15){
+    console.log("warm")
+
+}else {
+    console.log("hot")
+
+}
