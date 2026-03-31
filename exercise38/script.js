ab9fdd65bc0c2922163113816a0e31eea520f6b7
isoorapp <isoorapp@gmail.com>
2026-03-30 20:43:44 -0500

    add exercise38

---
+++ exercise38/script.js
@@ -0,0 +1,8 @@
+
+const toggleButton=document.querySelector(".toggle-button")
+const navbar=document.querySelector(".navbar")
+
+toggleButton.addEventListener('click', function(){
+
+    navbar.classList.toggle("active")
+})
